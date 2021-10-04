2bb791e343eb3c1581c7643820bf0bf0257a9c56
Diego Gonzalez <45858@psdschool.org>
2021-10-04 11:34:14 -0600

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Diego Gonzalez on 10/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
