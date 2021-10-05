a89cc583537e9706dce41fc584ea542e4b72df1a
Diego Gonzalez <45858@psdschool.org>
2021-10-05 11:46:44 -0600

    Final About Me

--- About Me/ViewController.swift
+++ About Me/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import SafariServices
+@IBDesignable extension UIButton {
 
-class ViewController: UIViewController {
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+class ViewController: UIViewController, SFSafariViewControllerDelegate  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func favoriteColor(_ sender: Any)
+    {
+        self.view.backgroundColor=UIColor.blue
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func clearButton(_ sender: Any)
+    {
+        self.view.backgroundColor=UIColor.white
+    }
+    
+    @IBAction func myUrl(_ sender: Any)
+    {
+        let fullURLPath = "https://www.truecolorsintl.com"
+        let myUrl1 = URL(string: fullURLPath)
+        let svc = SFSafariViewController(url: myUrl1!)
+        svc.delegate = self
+        self.present(svc, animated: true, completion: nil)
+    }
 }
 
